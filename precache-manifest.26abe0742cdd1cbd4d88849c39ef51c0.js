014f4e738d7e4629a1a6b21f6f3d3d00c2a4fc28
Tucsky <tucsky@pm.me>
2021-12-06 18:53:11 +0100

    gh-pages

---
+++ precache-manifest.26abe0742cdd1cbd4d88849c39ef51c0.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52840f25079672cb0e4ae495a20f405",
+    "url": "/aggr/android-chrome-192x192.png"
+  },
+  {
+    "revision": "66da64cd54beba64b10e7f5c2e3d12e5",
+    "url": "/aggr/android-chrome-512x512.png"
+  },
+  {
+    "revision": "583704002e6c9b875237b890b5609631",
+    "url": "/aggr/apple-touch-icon.png"
+  },
+  {
+    "revision": "eb66093e7080f5e3b76330e737b563b5",
+    "url": "/aggr/browserconfig.xml"
+  },
+  {
+    "revision": "2bfd6e6cf7f98a8e2d76",
+    "url": "/aggr/css/app.055eb982.css"
+  },
+  {
+    "revision": "a04a99211da974612eab08a55a6ec5e5",
+    "url": "/aggr/favicon-16x16.png"
+  },
+  {
+    "revision": "8ab615f338ec790358c2d260b047daee",
+    "url": "/aggr/favicon-32x32.png"
+  },
+  {
+    "revision": "4677a80cfdff03b5be754a1bf972812f",
+    "url": "/aggr/fonts/icon.4677a80c.woff"
+  },
+  {
+    "revision": "dee39ae5fedcf7897cc595460e4bbdfc",
+    "url": "/aggr/fonts/icon.dee39ae5.ttf"
+  },
+  {
+    "revision": "485a12cb6c7a339b88df4bc73b8c6fbe",
+    "url": "/aggr/img/BINANCE.485a12cb.svg"
+  },
+  {
+    "revision": "aff1e08ed440c9307c58ca8ed63ab9e7",
+    "url": "/aggr/img/BINANCE_FUTURES.aff1e08e.svg"
+  },
+  {
+    "revision": "485a12cb6c7a339b88df4bc73b8c6fbe",
+    "url": "/aggr/img/BINANCE_US.485a12cb.svg"
+  },
+  {
+    "revision": "4a8ca8e7ec465c4aa3bb531556a5b3fd",
+    "url": "/aggr/img/BITFINEX.4a8ca8e7.svg"
+  },
+  {
+    "revision": "447dea3806ca725aecdc63c696f37260",
+    "url": "/aggr/img/BITMEX.447dea38.svg"
+  },
+  {
+    "revision": "31b40fd5c92d4ddc002810dcb33ebac2",
+    "url": "/aggr/img/BITSTAMP.31b40fd5.svg"
+  },
+  {
+    "revision": "353cd67dab9beb87e1ffc31ded241257",
+    "url": "/aggr/img/BYBIT.353cd67d.svg"
+  },
+  {
+    "revision": "8a95d742e64da26988401306f5e2927c",
+    "url": "/aggr/img/COINBASE.8a95d742.svg"
+  },
+  {
+    "revision": "21f26f1e93db26859722f6d63aed5d28",
+    "url": "/aggr/img/DERIBIT.21f26f1e.svg"
+  },
+  {
+    "revision": "bca7826492aa3d8d6772df6a9162094a",
+    "url": "/aggr/img/FTX.bca78264.svg"
+  },
+  {
+    "revision": "c82e309c4720a6d17045aa34a183e772",
+    "url": "/aggr/img/HITBTC.c82e309c.svg"
+  },
+  {
+    "revision": "44809724927c44dda47c16f3dd91268c",
+    "url": "/aggr/img/HUOBI.44809724.svg"
+  },
+  {
+    "revision": "9729884464403eeccd32be6fe6730116",
+    "url": "/aggr/img/KRAKEN.97298844.svg"
+  },
+  {
+    "revision": "5d20779306867fdde423e1ccf4b0947b",
+    "url": "/aggr/img/OKEX.5d207793.svg"
+  },
+  {
+    "revision": "4a159b281ef1968d056a02b44c881ac2",
+    "url": "/aggr/img/PHEMEX.4a159b28.svg"
+  },
+  {
+    "revision": "ba8cf03bbcebd4b15c459c565cd62e4b",
+    "url": "/aggr/img/POLONIEX.ba8cf03b.svg"
+  },
+  {
+    "revision": "681f4a7918dfb8a74d783997b6279120",
+    "url": "/aggr/img/icon.681f4a79.svg"
+  },
+  {
+    "revision": "deede9a467ee8148af52f3f838d8c92b",
+    "url": "/aggr/index.html"
+  },
+  {
+    "revision": "4696964b9d02d862c38768d91699ec22",
+    "url": "/aggr/js/aggregator.82cb50c7.worker.js"
+  },
+  {
+    "revision": "2bfd6e6cf7f98a8e2d76",
+    "url": "/aggr/js/app.93b06070.js"
+  },
+  {
+    "revision": "686150511902e71837c1",
+    "url": "/aggr/js/chunk-vendors.bfab2036.js"
+  },
+  {
+    "revision": "6c554171d67a46482cf8e623a368e579",
+    "url": "/aggr/manifest.json"
+  },
+  {
+    "revision": "2d1bc7e6b11d8505fc5c04ae90d27371",
+    "url": "/aggr/mstile-144x144.png"
+  },
+  {
+    "revision": "36752d2829fff48d3b84cd2e9bac3b83",
+    "url": "/aggr/mstile-150x150.png"
+  },
+  {
+    "revision": "d723637642ba8f50a191e7a5bd6428f7",
+    "url": "/aggr/mstile-310x150.png"
+  },
+  {
+    "revision": "30107930797b320210e349ed601a9dd9",
+    "url": "/aggr/mstile-310x310.png"
+  },
+  {
+    "revision": "57fe6e3c59e6887fab1e7ab8bff2649a",
+    "url": "/aggr/mstile-70x70.png"
+  },
+  {
+    "revision": "af343de2aa6964387ef1b41673938435",
+    "url": "/aggr/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "ae89a1adf0f676ff0fe9e5d110badbd0",
+    "url": "/aggr/site.webmanifest"
+  },
+  {
+    "revision": "ca147f87437593045245d65e419cd024",
+    "url": "/aggr/sw.js"
+  }
+]);
